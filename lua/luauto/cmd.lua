182ff02d656a2f317e6f724199c46a20a804f12d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-08 15:02:12 -0500

    Add EmmyLua annotations
    Followed the how-to guide on `tjdevries/tree-sitter-lua` that was linked
    in the `nvim-telescope/telescope.nvim` contributing guidelines.

--- lua/luauto/cmd.lua
+++ lua/luauto/cmd.lua
@@ -8,6 +8,20 @@ local api = {
 }
 
 
+--- Add a callback/command to the list of commands and callbacks that Neovim
+--- will execute automatically on one or more events. Returns the id of the
+--- newly created autocommand.
+---@param tbl table: dictionary of autocommand options
+---@field cmd string: vim command to execute; cannot be used with cb
+---@field command string: alias for cmd
+---@field cb string|function: Lua function to call or name of a Vimscript function to call
+---@field callback string|function: alias for cb
+---@field on string|table: event or events to register this autocommand
+---@field group string|number: autocommand group name or id to match against
+---@field desc string: description of the autocommand
+---@field once boolean: run the autocommand only once; defaults to false
+---@field nested boolean: run nested autocommands; defaults to false
+---@return number: integer id of the created autocommand
 function M.add(tbl)
   if type(tbl) ~= "table" then error("expects a table as an argument", 2) end
   local cmd = tbl[1] or tbl.cmd or tbl.command
@@ -27,15 +41,31 @@ function M.add(tbl)
 end
 
 
+--- Delete an autocommand given its id.
+---@param id number: id of the autocommand to delete
 function M.del(id)
 end
 
 
-function M.get(tbl)
+--- Get all autocommands that match the corresponding opts.
+---@param opts table: dictionary containing at least one option to match against
+---@field group string|number: autocommand group name or id
+---@field event string|table: event(s)
+---@field pattern string|table: pattern(s); cannot be used with buffer
+---@field buffer number|table: buffer number(s); cannot be used with pattern
+---@return table: a list of autocommands matching the criteria
+function M.get(opts)
 end
 
 
-function M.clear(tbl)
+--- Clear all autocommands that match the corresponding opts.
+---@param opts table: dictionary containing at least one option to match against
+---@field group string|number: autocommand group name or id
+---@field event string|table: event(s)
+---@field pattern string|table: pattern(s); cannot be used with buffer
+---@field buffer number|table: buffer number(s); cannot be used with pattern
+---@return table: a list of autocommands matching the criteria
+function M.clear(opts)
 end
 
 
