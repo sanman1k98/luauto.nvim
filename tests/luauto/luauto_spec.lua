1530bc19a2554cb1cf93a3a13096b6bf520f84cd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-12 11:31:47 -0500

    Implement test for main `luauto` module

--- tests/luauto/luauto_spec.lua
+++ tests/luauto/luauto_spec.lua
@@ -1,4 +1,8 @@
 describe("The `luauto` module", function()
-  pending("returns a table that can access its submodules", function()
+  it("returns a table that can access its submodules", function()
+    local luauto = require "luauto"
+    assert.is_truthy(luauto.cmd)
+    assert.is_truthy(luauto.group)
+    assert.is_truthy(luauto.event)
   end)
 end)
