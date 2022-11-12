43a580fddc418c8bac0bb2db54969f876bf03091
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-12 12:09:46 -0500

    Test adding an autocmd to a group
    It works! The function call makes use of the `group` submodule and the
    `cmd` submodule's "add" function!

--- tests/luauto/luauto_spec.lua
+++ tests/luauto/luauto_spec.lua
@@ -1,8 +1,38 @@
+local au = require "luauto"
+local api = vim.api
+local print = vim.pretty_print
+
+
 describe("The `luauto` module", function()
   it("returns a table that can access its submodules", function()
-    local luauto = require "luauto"
-    assert.is_truthy(luauto.cmd)
-    assert.is_truthy(luauto.group)
-    assert.is_truthy(luauto.event)
+    assert.is_truthy(au.cmd)
+    assert.is_truthy(au.group)
+    assert.is_truthy(au.event)
+  end)
+
+  it("can declaratively add an autocommand to an autogroup", function()
+    local test
+    assert.has_no.errors(function()
+      test = au.group.testing:add {
+        on = "BufEnter",
+        cb = function() vim.notify "Hello! Testing!" end,
+        once = true,
+        desc = "Created for testing",
+      }
+    end)
+    assert.is_true(type(test) == "number")
+    local cmds = api.nvim_get_autocmds {
+      group = "testing",
+      event = "BufEnter",
+    }
+    local contains_test = false
+    for _, c in ipairs(cmds) do
+      if c.id == test then
+        print(c)
+        contains_test = true
+        break
+      end
+    end
+    assert.is_true(contains_test)
   end)
 end)
