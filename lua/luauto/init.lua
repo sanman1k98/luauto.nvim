8d78a7e1b8d3d8a5368b580b78e69d6b7ef4717c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-19 02:15:37 -0500

    Completely redo `luauto` again
    But we got that second example snippet test working tho!
    
    I think this is finally headed in the right direction cause that example
    showing what it looks like defining a group feels right. Like its got
    that familiar Vimscript DSL and its Lua at the same time.
    
    Now I think we can flesh out the rest of the tests and trim the fat here
    and there goni forward. I don't think there is gonna be any more major
    changes from this implementation.

--- lua/luauto/init.lua
+++ lua/luauto/init.lua
@@ -1,279 +1,206 @@
-local M = {}  -- "auto"
+local M = {
+  cmd = {}, group = {}
+}
 local Group, Event = {}, {}
--- stores names of objects
-local info = setmetatable({}, { __mode = "k" })
+
+local group_mem = setmetatable({}, { __mode = "v" })
+local event_mem = setmetatable({}, { __mode = "v" })
+local private = setmetatable({}, { __mode = "k" })
 local api, validate = vim.api, vim.validate
 
 
 
---- Wrapper around autocommand API functions.
---- - cmd.del = nvim_del_autocmd
---- - cmd.clear = nvim_clear_autocmds
---- - cmd.exec = nvim_exec_autocmds
---- - cmd.get = nvim_get_autocmds
---- - cmd = nvim_create_autocmds
---
---- `cmd` can be called with the exact same arguments as `nvim_create_autocmd`
---- or if you are specifying a Vim command to be executed when this autocommand
---- triggers, you can pass it in as a string between the event and opts table
---- arguments.
-M.cmd = setmetatable({
-  del = api.nvim_del_autocmd,
-  clear = api.nvim_clear_autocmds,
-  exec = api.nvim_exec_autocmds,
-  get = api.nvim_get_autocmds,
-}, {
-  __call = function(_, ...)
-    local args = {...}
-    if type(args[2]) == "table" then
-      return api.nvim_create_autocmd(args[1], args[2])
-    end
-    local event, command, opts = ...
-    validate {
-      event = { event, {"s", "t"} },
-      command = { command, "s" },
-      opts = { opts, "t", true },
-    }
-    opts = opts or {}
-    opts.command = command
-    return api.nvim_create_autocmd(event, opts)
-  end,
-})
+function M.cmd:del(id)
+  api.nvim_del_autocmd(id)
+end
 
+function M.cmd:clear(opts)
+  api.nvim_clear_autocmds(opts)
+end
 
+function M.cmd:get(opts)
+  return api.nvim_get_autocmds(opts)
+end
 
-M.cb = function(event, cb, opts)
-  validate {
-    event = { event, {"s", "t"} },
-    callback = { cb, {"s", "f"} },
-    opts = { opts, "t", true },
+function M.cmd:exec(event, opts)
+  api.nvim_exec_autocmds(opts)
+end
+
+do
+  local function event_proxy(name)
+    name = name:lower()     -- event names are case-insensitive
+    if event_mem[name] then return event_mem[name] end
+    local proxy = {}
+    event_mem[name] = proxy
+    private[proxy] = { event = name }
+    return setmetatable(proxy, Event)
+  end
+
+  local mt = {
+    __index = function(_, k)
+      return event_proxy(k)
+    end,
+
+    __call = function(_, event, action, opts)
+      validate {
+        event = { event, {"s", "t"} },
+        action = { action, {"s", "f"} },
+        opts = { opts, "t", true },
+      }
+      opts = opts or {}
+      if type(action) == "string" and action[1] == ":" then
+        opts.command = action
+      else
+        opts.callback = action
+      end
+      return api.nvim_create_autocmd(event, opts)
+    end,
   }
-  local opts = opts or {}
-  opts.callback = cb
-  return api.nvim_create_autocmd(event, opts)
-end
 
-
-
-function Group:_proxy(name)
-  local p = {}
-  info[p] = name
-  return setmetatable(p, self)
-end
-
-function Group:create()
-  local id = api.nvim_create_augroup(info[self], { clear = false })
-  return id
-end
-
-function Group:clear(opts)
-  validate { opts = { opts, "t", true }, }
-  if not opts then
-    return api.nvim_create_augroup(info[self], { clear = true })
-  else
-    opts.group = info[self]
-    M.cmd.clear(opts)
-  end
-end
-
-function Group:del()
-  api.nvim_del_augroup_by_name(info[self])
-  self = nil
-end
-
-function Group:get_name()
-  return info[self]
-end
-
-function Group:get_id()
-  return api.nvim_create_augroup(info[self], { clear = false })
-end
-
-function Group:get_cmds(opts)
-  validate { opts = { opts, "t", true }, }
-  opts.group = info[self]
-  return M.cmd.get(opts)
-end
-
-function Group:cmd(event, ...)
-  local arg2, arg3 = ...
-  validate {
-    event = { event, {"s", "t"} },
-    arg2 = { arg2, {"t", "s" } },
-    arg3 = { arg3, "t", true },
-  }
-  if type(arg2) == "table" then
-    arg2.group = info[self]
-    return M.cmd(event, arg2)
-  else
-    local opts = arg3 or {}
-    opts.command = arg2
-    opts.group = info[self]
-    return M.cmd(event, opts)
-  end
-end
-
-function Group:cb(event, cb, opts)
-  validate {
-    event = { event, {"s", "t"} },
-    callback = { cb, {"s", "f"} },
-    opts = { opts, "t", true },
-  }
-  local opts = opts or {}
-  opts.callback = cb
-  return M.cmd(event, opts)
-end
-
-function Group:define(cmds)
-  validate { autocmds = { cmds, function (x)
-    return x == nil or vim.tbl_islist(x)
-  end, "list" } }
-  local autocmd_ids = {}
-  for i, cmd in ipairs(cmds) do
-    local event, opts = cmd[1], cmd[2]
-    autocmd_ids[i] = Group.cmd(self, event, opts)
-  end
-  return autocmd_ids
-end
-
-function Group:__index(k)
-  if k == "id" then return Group.get_id(self)
-  elseif k == "name" then return Group.get_name(self)
-  else
-    local v = rawget(Group, k)
-    rawset(self, k, v)
-    return v
-  end
-end
-
-function Group:__newindex(k, v)
-  if k == "id" or k == "name" then
-    error("attempting to modify a read-only field", 2)
-  else
-    rawset(self, k, v)
-  end
-end
-
-function Group:__call(...)
-  return Group.define(self, ...)
-end
-
-M.group = setmetatable({}, {
-  __index = function(self, k)
-    local v = Group:_proxy(k)
-    rawset(self, k, v)
-    return v
-  end,
-  __mode = "v"
-})
-
-
-
-function Event:_proxy(name)
-  local p = {}
-  info[p] = name
-  return setmetatable(p, self)
-end
-
-function Event:cmd(...)
-  return M.cmd(info[self], ...)
-end
-
-function Event.cb(...)
-  return M.cb(info[self], ...)
-end
-
-function Event:exec(opts)
-  validate { opts = { opts, "t", true } }
-  opts = opts or {}
-  return M.cmd.exec(info[self], opts)
-end
-
-function Event:get_cmds(opts)
-  validate { opts = { opts, "t", true } }
-  opts = opts or {}
-  opts.event = info[self]
-  return M.cmd.get(opts)
+  M.cmd = setmetatable(M.cmd, mt)
 end
 
 function Event:clear(opts)
   validate { opts = { opts, "t", true } }
   opts = opts or {}
-  opts.event = info[self]
-  return M.cmd.clear(opts)
+  M.cmd.clear(opts)
 end
 
-function Event:is_ignored()
-  for _, event in ipairs(vim.opt.eventignore:get()) do
-    event = string.lower(event)
-    if event == info[self] then
-      return true
-    end
-  end
-  return false
+function Event:get(opts)
+  validate { opts = { opts, "t", true } }
+  opts = opts or {}
+  opts.event = private[self].event
+  return M.cmd:get(opts)
 end
 
-function Event:set_ignore(flag)
-  if flag then vim.opt.eventignore:append(info[self])
-  else vim.opt.eventignore:remove(info[self]) end
+function Event:exec(opts)
+  validate { opts = { opts, "t", true } }
+  M.cmd:exec(private[self].event, opts)
 end
 
-function Event:get_name()
-  return info[self]
+function Event:info()
+  return private[self]
 end
 
-function Event:__index(k)
-  if k == "name" then
-    return info[self]
-  elseif k == "ignore" then
-    return Event.is_ignored(self)
+function Event:__call(action, opts)
+  validate {
+    action = { action, {"s", "f"} },
+    opts = { opts, "t", true },
+  }
+  return M.cmd(private[self].event, action, opts)
+end
+
+Event.__index = Event
+
+function Group:create()
+  return api.nvim_create_augroup(private[self].group, { clear = false })
+end
+
+function Group:info()
+  local info = {}
+  info.group = private[self].group
+  info.id = Group.create(self)
+  return info
+end
+
+function Group:clear(opts)
+  validate { opts = { opts, "t", true } }
+  if not opts then
+    return api.nvim_create_augroup(private[self].group, { clear = true })
   else
-    local v = Event[k]
-    rawset(self, k, v)
-    return v
+    M.cmd:clear(opts)
   end
 end
 
-function Event:__newindex(k, v)
-  if k == "name" then
-    error("attempting to modify a read-only field", 2)
-  elseif k == "ignore" then
-    Event.set_ignore(self, v)
+function Group:del()
+  api.nvim_del_augroup_by_name(private[self].group)
+  self = nil
+end
+
+function Group:get(opts)
+  validate { opts = { opts, "t", true } }
+  opts = opts or {}
+  opts.group = private[self].group
+  return M.cmd:get(opts)
+end
+
+function Group:exec(event, opts)
+  validate {
+    event = { event, {"s", "t"} },
+    opts = { opts, "t", true },
+  }
+  opts = opts or {}
+  opts.group = private[self].group
+  M.cmd:exec(event, opts)
+end
+
+function Group:define(...)
+  Group.create(self)
+  local args = {...}
+  if vim.is_callable(args[1]) then
+    local spec_func = args[1]
+    local au = setmetatable({
+      clear = function()
+        Group.clear(self)
+      end,
+    }, {
+      __index = function(_, k)
+        return (function(action, opts)
+          validate {
+            action = { action, {"s", "f"} },
+            opts = { opts, "t", true },
+          }
+          opts = opts or {}
+          opts.group = private[self].group
+          M.cmd(k, action, opts)
+        end)
+      end,
+    })
+    return spec_func(au)
+  else
+    for _, autocmd in ipairs(args) do
+      assert(type(autocmd) == "table")
+      M.cmd(autocmd[1], autocmd[2], autocmd[3])
+    end
+  end
+end
+
+function Group:__index(k)
+  if k == "id" then
+    return Group.create(self)
+  else
+    return Group[k]
+  end
+end
+
+function Group:__newindex(k, v)
+  if k == "id" then
+    error("attempting to modify a read-only field: " .. k, 2)
   else
     rawset(self, k, v)
   end
 end
 
---- Shorthand for creating an autocommand for the event.
-function Event:__call(...)
-  return Event.cmd(self, ...)
+Group.__call = Group.define
+
+do
+  local function group_proxy(name)
+    if group_mem[name] then return group_mem[name] end
+    local proxy = {}
+    group_mem[name] = proxy
+    private[proxy] = { group = name }
+    return setmetatable(proxy, Group)
+  end
+
+  local mt = {
+    __index = function(_, k)
+      return group_proxy(k)
+    end,
+  }
+
+  M.group = setmetatable(M.group, mt)
 end
 
 
---- Indexable by event names, which are case-insensitive.
-M.event = setmetatable({}, {
-  __index = function(self, key)
-    local proxy = rawget(self, key:lower())
-    if not proxy then
-      proxy = Event:_proxy(key:lower())
-      rawset(self, key:lower(), proxy)
-    end
-    rawset(self, key, proxy)
-    return proxy
-  end,
-  __mode = "v",
-})
 
-
-
-return setmetatable(M, {
-  __index = function(self, k)
-    if k == "user" then
-      local v = require "luauto.user"
-      rawset(self, k, v)
-      return v
-    else
-      return nil
-    end
-  end,
-})
+return M
