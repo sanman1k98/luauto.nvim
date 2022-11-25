9f01f8755fc532f2f4815fc9b3cf77c5e6495b13
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-25 17:24:36 -0500

    Implement some tests

--- tests/augroup_spec.lua
+++ tests/augroup_spec.lua
@@ -1,50 +1,85 @@
-local helpers = require "tests.helpers"
+local augroup = require("luauto").group
 
+local helpers = require "tests.helpers"
 local api = helpers.api
-local ok = helpers.ok
-local not_ok = helpers.not_ok
-local truthy = helpers.truthy
-local falsy = helpers.falsy
-local eq = helpers.eq
-local same = helpers.same
+
+local truthy = assert.is_truthy
+local falsy = assert.is_falsy
+local ok = assert.has_no.errors
+local not_ok = assert.has_errors
+local same = assert.are_same              -- deep comparison
+local eq = assert.is_equal                -- compare by value or by reference
+local pp = vim.pretty_print
 
 
 
 describe("a table to manage an augroup", function()
+
+  local testgroup = augroup.testgroup
+
   describe("has a method", function()
     pending("'create'", function()
+      truthy(testgroup.create)
+      assert.is_function(testgroup.create)
     end)
 
     pending("'clear'", function()
+      truthy(testgroup.clear)
+      assert.is_function(testgroup.clear)
     end)
 
     pending("'get'", function()
+      truthy(testgroup.get)
+      assert.is_function(testgroup.get)
     end)
 
     pending("'del'", function()
+      truthy(testgroup.del)
+      assert.is_function(testgroup.del)
     end)
 
     pending("'exec'", function()
+      truthy(testgroup.exec)
+      assert.is_function(testgroup.exec)
     end)
 
     pending("'define'", function()
-    end)
-
-    pending("''", function()
+      truthy(testgroup.define)
+      assert.is_function(testgroup.define)
     end)
   end)
 
   describe("has a read-only property", function()
     pending("'id'", function()
+      truthy(testgroup.id)
+      assert.number(testgroup.id)
+      not_ok(function()
+        testgroup.id = 0
+      end)
     end)
 
     pending("'name'", function()
+      truthy(testgroup.name)
+      assert.string(testgroup.name)
+      not_ok(function()
+        testgroup.name = "testgroup2"
+      end)
     end)
 
     pending("'scope'", function()
+      truthy(testgroup.scope)
+      assert.table(testgroup.scope)
+      not_ok(function()
+        testgroup.scope = {}
+      end)
     end)
 
     pending("'autocmd'", function()
+      truthy(testgroup.autocmd)
+      assert.table(testgroup.autocmd)
+      not_ok(function()
+        testgroup.autocmd = {}
+      end)
     end)
   end)
 
