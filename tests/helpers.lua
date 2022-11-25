5d29ce98ebfbfa25df5069c9b3c0f6b0dbba2878
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-25 17:26:33 -0500

    Add helper funcs to create/clear test autocmds

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -8,17 +8,26 @@ M.api = setmetatable({}, {
   end,
 })
 
-function M.clear(group)
+function M.clear_all(group)
   api.clear_autocmds { group = group }
+  api.clear_autocmds {}
 end
 
-M.truthy = assert.truthy
-M.falsy = assert.falsy
-M.ok = assert.has_no.errors
-M.not_ok = assert.has.errors
-M.same = assert.same
-M.eq = assert.equal
+function M.get_test_autocmds(group)
+  return api.get_autocmds { event = "User", group = group }
+end
 
-
+function M.create_test_autocmds(group)
+  local cb = function() end
+  for i = 1, 10 do
+    api.create_autocmd("User", {
+      callback = cb,
+      group = group,
+      pattern = string.format("testpattern%d", i),
+      desc = string.format("test autocmd %d", i),
+      once = true,
+    })
+  end
+end
 
 return M
