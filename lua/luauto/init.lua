108ccf02c28260968bcedb16be2e274e529e88dd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-25 16:11:09 -0500

    Committing ideas again

--- lua/luauto/init.lua
+++ lua/luauto/init.lua
@@ -1,28 +1,65 @@
 local M = {}
 
-local autocmd = {}
-
-local events;
-local eventobj_mt, events_mt = {}, {}
-
-local augroup = {}
+local autocmd_mt = {}
+local event_mt, eventset_mt = {}, {}
 local group_mt = {}
+
+local get_eventset_obj, get_group_obj, get_autocmd_obj
 
 local attr = setmetatable({}, { __mode = "k" })
 local a, validate = vim.api, vim.validate
 
----@param event string|table: event or events to register this autocmd for
----@param action function|string: Vim command prefixed with ":", the name of a Vim function, or a Lua function
----@param opts table|nil: a dictionary of autocmd options
----@return id number: integer id of the created autocmd
----@see nvim_create_autocmd()
-function autocmd:create(event, action, opts)
+
+
+--
+--
+--
+--
+--
+
+local function validate_opts(opts)
+  validate {
+    opts = { opts, "t" },
+    group = { opts.group, {"s", "n"}, true },
+    buffer = { opts.buffer, "n", true },
+  }
+  if opts.buffer and opts.pattern then
+    error("cannot specify both pattern and buffer", 2)
+  end
+end
+
+local function keygen(opts)
+  validate_opts(opts)
+  return vim.inspect(opts)
+end
+
+local function set_opts(self, opts, set_event)
+  local opts = self.scope
+  if not opts then
+    return scope
+  else -- set event field unless otherwise specified
+    opts.event = (set_event == nil or set_event == true) and self.event or opts.event
+    opts.group = scope.group
+    opts.buffer = scope.buffer
+  end
+  return opts
+end
+
+
+
+--
+--
+--        autocmd_mt
+--
+--
+
+function autocmd_mt:create(event, action, opts)
   validate {
     event = { event, {"s", "t"} },
     action = { action, {"s", "f"} },
     opts = { opts, "t", true },
   }
-  opts = opts or {}
+  opts = set_scope(self, opts, false)
   if type(action) == "string" and action[1] == ":" then
     opts.command = action:sub(2)
   else
@@ -31,161 +68,186 @@ function autocmd:create(event, action, opts)
   return a.nvim_create_autocmd(event, opts)
 end
 
-
-
-local function scoped_opts(self, opts)
-  if not opts then opts = attr[self].scope
-  else
-    opts.group = attr[self].scope.group
-    opts.buffer = attr[self].scope.buffer
-  end
-  return opts
-end
- 
----@see nvim_get_autocmds()
-function eventobj_mt:get(opts)
-  validate { opts = { opts, "t", true } }
-  opts = scoped_opts(self, opts)
-  opts.event = attr[self].name
-  return a.nvim_get_autocmds(opts)
-end
-
----@see nvim_exec_autocmds()
-function eventobj_mt:exec(opts)
-  validate { opts = { opts, "t", true } }
-  return a.nvim_exec_autocmds(attr[self].name, scoped_opts(self, opts))
-end
-
 ---@see nvim_clear_autocmds()
-function eventobj_mt:clear(opts)
+function autocmd_mt:clear(opts)
   validate { opts = { opts, "t", true } }
-  opts = scoped_opts(self, opts)
-  opts.event = attr[self].name
+  opts = set_scope(self, opts)
   return a.nvim_clear_autocmds(opts)
 end
 
-function eventobj_mt:__index(k)
-  if k == "name" or k == "event_name" then
-    return attr[self].name
+---@see nvim_exec_autocmds()
+function autocmd_mt:exec(event, opts)
+  validate {
+    event = { event, {"s", "t"} },
+    opts = { opts, "t", true },
+  }
+  opts = set_scope(self, opts, false)
+  a.nvim_exec_autocmds(event, opts)
+end
+
+--- Can specify multiple buffer numbers
+---@see nvim_get_autocmds()
+function autocmd_mt:get(opts)
+  validate { opts = { opts, "t", true } }
+  opts = set_scope(self, opts)
+  return a.nvim_get_autocmds(opts)
+end
+
+function autocmd_mt:del(...)
+  a.nvim_del_autocmd(...)
+end
+
+autocmd_mt.__call = autocmd_mt.create
+
+function autocmd_mt:__index(k)
+  return attr[self][k] or rawget(autocmd_mt, k) or attr[self].eventset[k]
+end
+
+function autocmd_mt:__newindex(k, v)
+  if k == "group" or k == "buffer"
+    then error("attemping to modify a read-only field", 2)
   else
-    return rawget(eventobj_mt, k)
+    rawset(self, k, v)
   end
 end
 
---- Create an autocmd for event.
----@param action function|string: a Vim command prefixed with ":", or a callback func
----@param opts table|nil: a dictionary of autocmd options
----@return id number: integer id of the created autocmd
----@see nvim_create_autocmd()
-function eventobj_mt:__call(action, opts)
-  return autocmd:create(attr[self].name, action, scoped_opts(self, opts))
+
+
+--
+--
+--        event_mt, eventset_mt
+--
+--
+
+event_mt.get = autocmd_mt.get
+event_mt.clear = autocmd_mt.clear
+function event_mt:exec(...) autocmd_mt.exec(self, self.name, ...) end
+function event_mt:__call(...) return autocmd_mt.create(self, self.name, ...) end
+
+function event_mt:__index(k)
+  if k == "name" or k == "event" then
+    return attr[self].event
+  elseif k == "group" then
+    return attr[self].group
+  elseif k == "buffer" then
+    return attr[self].buffer
+  else
+    return rawget(event_mt, k)
+  end
 end
 
-function events_mt:__index(k)
+function event_mt:__newindex(k, v)
+  if k == "name"
+    or k == "event"
+    or k == "group"
+    or k == "buffer"
+    then error("attemping to modify a read-only field", 2)
+  else
+    rawset(self, k, v)
+  end
+end
+
+function eventset_mt:__index(k)
   local event = rawget(self, k:lower())
   if not event then
     event = {}
     attr[event] = {
-      name = k,
-      scope = attr[self]
+      group = attr[self].group,
+      buffer = attr[self].buffer,
+      event = k:lower(),
     }
-    event = setmetatable(event, eventobj_mt)
+    event = setmetatable(event, event_mt)
     rawset(self, k:lower(), event)
   end
   rawset(self, k, event)
   return event
 end
 
-events_mt.__mode = "v"
+eventset_mt.__mode = "v"
+
+
+
+--
+--
+--        get_eventset_obj, get_autocmd_obj
+--
+--
 
 do
   local mem = setmetatable({}, { __mode = "v" })
 
-  ---@param scope table|nil: the scope in which to create, clear, get, and exec autocmds for each event
-  ---@field group string|nil: name of an autogroup
-  ---@field buffer number|nil: a buffer number
-  ---@return string: a key to index memoized results in the "mem" table
-  local function keygen(scope)
-    if not scope or vim.tbl_isempty(scope) then return "aug END" end
-    local augroup = ("aug %s"):format(scope.group or "END")
-    local buffer = scope.buffer and (" <buffer=%d>"):format(scope.buffer) or ""
-    return augroup .. buffer
-  end
-
-  local function get_events(scope)
-    validate { scope = { scope, "t", true } }
-    local key = keygen(scope)
+  get_eventset_obj = function(opts)
+    validate { opts = { opts, "t" } }
+    local key = keygen(opts)
     if mem[key] then return mem[key] end
-    local t = {}
-    mem[key] = t
-    attr[t] = scope or {}
-    return setmetatable(t, events_mt)
+    local set = {}
+    mem[key] = set
+    attr[set] = {
+      group = opts.group,
+      buffer = opts.buffer,
+    }
+    return setmetatable(set, eventset_mt)
   end
+end
 
-  events = get_events
+do
+  local mem = setmetatable({}, { __mode = "v" })
+
+  get_autocmd_obj = function(opts)
+    validate { opts = { opts, "t" } }
+    local key = keygen(opts)
+    if mem[key] then return mem[key] end
+    local au = {}
+    mem[key] = au
+    attr[au] = {
+      group = opts.group,
+      buffer = opts.buffer,
+      eventset = get_eventset_obj(opts)
+    }
+    return setmetatable(au, autocmd_mt)
+  end
 end
 
 
 
-function autocmd:clear(...) a.nvim_clear_autocmds(...) end
-function autocmd:del(...) a.nvim_del_autocmd(...) end
-function autocmd:exec(...) a.nvim_exec_autocmds(...) end
-function autocmd:get(...) return a.nvim_get_autocmds(...) end
+--
+--
+--        group_mt
+--
+--
 
-M.cmd = setmetatable(autocmd, {
-  __index = events(nil),
-  __call = autocmd.create,
-})
+group_mt.get = autocmd_mt.get
+group_mt.exec = autocmd_mt.exec
 
-
-
---- Create this autogroup without clearing it.
----@return integer: this group's id
 function group_mt:create()
-  return a.nvim_create_augroup(attr[self].group_name, { clear = false })
+  return a.nvim_create_augroup(self.name, { clear = false })
 end
 
---- Clear the autogroup.
----@return integer: this group's id
 function group_mt:clear(opts)
-  validate { opts = { opts, "t", true } }
-  if not opts or vim.tbl_isempty(opts) then
-    return a.nvim_create_augroup(attr[self].group_name, { clear = true })
+  if not opts then
+    return a.nvim_create_augroup(self.name, { clear = true })
   else
-    opts = opts or {}
-    opts.group = attr[self].group_name
-    a.nvim_clear_autocmds(opts)
+    autocmd_mt.clear(self, opts)
   end
 end
 
---- Delete the autogroup and remove this object from the table "mem".
 function group_mt:del()
-  a.nvim_del_augroup_by_name(attr[self].group_name)
+  a.nvim_del_augroup_by_name(self.name)
   self = nil
 end
 
-function group_mt:get(opts)
-  validate { opts = { opts, "t", true } }
-  opts = opts or {}
-  opts.group = attr[self].group_name
-  return a.nvim_get_autocmds(opts)
-end
-
-function group_mt:exec(event, opts)
-  validate {
-    event = { event, {"s", "t"} },
-    opts = { opts, "t", true },
-  }
-  opts = opts or {}
-  opts.group = attr[self].group_name
-  return a.nvim_exec_autocmds(event, opts)
+function group_mt:define(spec)
+  validate { spec = { spec, "f" } }
+  return spec(self.au)
 end
 
 function group_mt:__index(k)
   if k == "id" then
     return group_mt.create(self)
-  elseif k == "name" or k == "group_name" then
-    return attr[self].group_name
+  elseif k == "name" or k == "group" then
+    return attr[self].scope.group
+  elseif k == "scope" then
+    return attr[self].scope
   elseif k == "au" or k == "autocmd" then
     return attr[self].autocmd
   else
@@ -196,7 +258,7 @@ end
 function group_mt:__newindex(k, v)
   if k == "id"
     or k == "name"
-    or k == "group_name"
+    or k == "scope"
     or k == "au"
     or k == "autocmd"
     then error("attempting to modify a read-only field", 2)
@@ -205,69 +267,38 @@ function group_mt:__newindex(k, v)
   end
 end
 
-function group_mt:__call(...)
-  group_mt.create(self)
-  local au = attr[self].autocmd
-  local spec = ...
-  if vim.is_callable(spec) then
-    return spec(au)
-  else
-    for _, cmd in ipairs{...} do
-      assert(type(cmd) == "table", "expecting arguments to be tables")
-      au(cmd[1], cmd[2], cmd[3])
-    end
-  end
-end
+group_mt.__call = group_mt.define
+
+
+
+--
+--
+--        get_group_obj
+--
+--
 
 do
   local mem = setmetatable({}, { __mode = "v" })
 
-  local function create_autocmd_tbl(group)
-    local au = {}
-    attr[au] = { group_name = attr[group].group_name }
-
-    au.clear = group_mt.clear
-    au.get = group_mt.get
-    au.exec = group_mt.exec
-    function au:del(...) a.nvim_del_autocmd(...) end
-
-    return setmetatable(au, {
-      __index = events({ group = attr[au].group_name }),
-      __call = function(self, event, action, opts)
-        validate { opts = { opts, "t", true } }
-        opts = opts or {}
-        opts.group = attr[self].group_name
-        return autocmd:create(event, action, opts)
-      end
-    })
+  get_group_obj = function(k)
+    validate { group_name = { k, "s" } }
+    if mem[k] then return mem[k] end
+    local group = {}
+    mem[k] = group
+    attr[group] = {
+      opts = {
+        group = k
+      }
+    }
+    attr[group].autocmd = get_autocmd_obj(attr[group].opts)
+    return setmetatable(group, group_mt)
   end
-
-  -- TODO: extract into a function that when given a name, returns an augroup
-  -- table with metatable `group_mt`.
-  --
-  -- Support passing "default", "end", and "END" as args to create an augroup
-  -- table for the default group.
-  --
-  --
-  --
-  -- I'm thinking about this because at this point, we're creating an "autocmd"
-  -- table for every group object that we create, and the "autocmd" table in
-  -- this module is pretty much the same thing but for the default group. I
-  -- think it's okay to be a little clever here and flesh out the logic for
-  -- creating an augroup object and its "autocmd" table, then use it for this
-  -- module.
-  M.group = setmetatable(augroup, {
-    __index = function(self, k)
-      if mem[k] then return mem[k] end
-      local group = {}
-      mem[k] = group
-      attr[group] = { group_name = k }
-      attr[group].autocmd = create_autocmd_tbl(group)
-      return setmetatable(group, group_mt)
-    end,
-  })
 end
 
 
+
+M.cmd = get_autocmd_obj {}
+M.group = setmetatable({}, { __index = function(_, k) return get_group_obj(k) end, })
+M.events = get_eventset_obj
 
 return M
