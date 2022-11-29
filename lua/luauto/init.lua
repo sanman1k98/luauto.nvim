bfe430a6b6c7d134d5dcfa2049e4ca5c942d3f66
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-11-29 11:31:29 -0500

    Committing ideas

--- lua/luauto/init.lua
+++ lua/luauto/init.lua
@@ -1,65 +1,47 @@
 local M = {}
 
-local autocmd_mt = {}
-local event_mt, eventset_mt = {}, {}
-local group_mt = {}
-
-local get_eventset_obj, get_group_obj, get_autocmd_obj
-
-local attr = setmetatable({}, { __mode = "k" })
 local a, validate = vim.api, vim.validate
 
+local AutocmdManager = {}
+local EventProxy = {}
+local GroupManager = {}
 
+local mem = setmetatable({}, { __mode = "v" })  -- memoizing table
+local private = setmetatable({}, { __mode = "k" })  -- private attributes for objects
 
---
---
---
---
---
+local context = {
+  new = function(self, opts)
+    validate { opts = { opts, "t", true } }
+    opts = opts or {}
+    local g = opts.group
+    opts.group = not (g == "END" or g == "end" or g == "default") and g or nil
+    return setmetatable(opts, self)
+  end,
 
-local function validate_opts(opts)
-  validate {
-    opts = { opts, "t" },
-    group = { opts.group, {"s", "n"}, true },
-    buffer = { opts.buffer, "n", true },
-  }
-  if opts.buffer and opts.pattern then
-    error("cannot specify both pattern and buffer", 2)
-  end
-end
+  merge = function(self, ...)
+    return vim.tbl_extend("keep", self, ...)
+  end,
 
-local function keygen(opts)
-  validate_opts(opts)
-  return vim.inspect(opts)
-end
-
-local function set_opts(self, opts, set_event)
-  local opts = self.scope
-  if not opts then
-    return scope
-  else -- set event field unless otherwise specified
-    opts.event = (set_event == nil or set_event == true) and self.event or opts.event
-    opts.group = scope.group
-    opts.buffer = scope.buffer
-  end
-  return opts
-end
+  force_merge = function(self, ...)
+    return vim.tbl_extend("force", self, ...)
+  end,
+}
 
 
 
 --
 --
---        autocmd_mt
+--        autocmd manager
 --
 --
 
-function autocmd_mt:create(event, action, opts)
+function AutocmdManager:__call(event, action, opts)
   validate {
     event = { event, {"s", "t"} },
     action = { action, {"s", "f"} },
     opts = { opts, "t", true },
   }
-  opts = set_scope(self, opts, false)
+  opts = private[self].ctx:merge(opts or {})
   if type(action) == "string" and action[1] == ":" then
     opts.command = action:sub(2)
   else
@@ -69,236 +51,183 @@ function autocmd_mt:create(event, action, opts)
 end
 
 ---@see nvim_clear_autocmds()
-function autocmd_mt:clear(opts)
+function AutocmdManager:clear(opts)
   validate { opts = { opts, "t", true } }
-  opts = set_scope(self, opts)
+  opts = private[self].ctx:merge(opts or {})
   return a.nvim_clear_autocmds(opts)
 end
 
 ---@see nvim_exec_autocmds()
-function autocmd_mt:exec(event, opts)
+function AutocmdManager:exec(event, opts)
   validate {
     event = { event, {"s", "t"} },
     opts = { opts, "t", true },
   }
-  opts = set_scope(self, opts, false)
+  opts = private[self].ctx:merge(opts or {})
   a.nvim_exec_autocmds(event, opts)
 end
 
---- Can specify multiple buffer numbers
 ---@see nvim_get_autocmds()
-function autocmd_mt:get(opts)
+function AutocmdManager:get(opts)
   validate { opts = { opts, "t", true } }
-  opts = set_scope(self, opts)
+  opts = private[self].ctx:force_merge(opts or {})
   return a.nvim_get_autocmds(opts)
 end
 
-function autocmd_mt:del(...)
-  a.nvim_del_autocmd(...)
+function AutocmdManager:del(id)
+  a.nvim_del_autocmd(id)
 end
 
-autocmd_mt.__call = autocmd_mt.create
-
-function autocmd_mt:__index(k)
-  return attr[self][k] or rawget(autocmd_mt, k) or attr[self].eventset[k]
-end
-
-function autocmd_mt:__newindex(k, v)
-  if k == "group" or k == "buffer"
-    then error("attemping to modify a read-only field", 2)
-  else
-    rawset(self, k, v)
-  end
+function AutocmdManager:__index(k)
+  return private[self].eventindex[k]
 end
 
 
 
 --
 --
---        event_mt, eventset_mt
+--        event
 --
 --
 
-event_mt.get = autocmd_mt.get
-event_mt.clear = autocmd_mt.clear
-function event_mt:exec(...) autocmd_mt.exec(self, self.name, ...) end
-function event_mt:__call(...) return autocmd_mt.create(self, self.name, ...) end
-
-function event_mt:__index(k)
-  if k == "name" or k == "event" then
-    return attr[self].event
-  elseif k == "group" then
-    return attr[self].group
-  elseif k == "buffer" then
-    return attr[self].buffer
-  else
-    return rawget(event_mt, k)
-  end
+function EventProxy:get(opts)
+  validate { opts = { opts, "t", true } }
+  opts = private[self].ctx:force_merge({ event = private[self].name }, opts or {})
+  return a.nvim_get_autocmds(opts)
 end
 
-function event_mt:__newindex(k, v)
-  if k == "name"
-    or k == "event"
-    or k == "group"
-    or k == "buffer"
-    then error("attemping to modify a read-only field", 2)
-  else
-    rawset(self, k, v)
-  end
+function EventProxy:clear(opts)
+  validate { opts = { opts, "t", true } }
+  opts = private[self].ctx:force_merge({ event = private[self].name }, opts or {})
+  a.nvim_clear_autocmds(opts)
 end
 
-function eventset_mt:__index(k)
-  local event = rawget(self, k:lower())
-  if not event then
-    event = {}
-    attr[event] = {
-      group = attr[self].group,
-      buffer = attr[self].buffer,
-      event = k:lower(),
-    }
-    event = setmetatable(event, event_mt)
-    rawset(self, k:lower(), event)
-  end
-  rawset(self, k, event)
-  return event
+function EventProxy:exec(opts)
+  AutocmdManager.exec(self, private[self].name, opts)
 end
 
-eventset_mt.__mode = "v"
-
-
-
---
---
---        get_eventset_obj, get_autocmd_obj
---
---
-
-do
-  local mem = setmetatable({}, { __mode = "v" })
-
-  get_eventset_obj = function(opts)
-    validate { opts = { opts, "t" } }
-    local key = keygen(opts)
-    if mem[key] then return mem[key] end
-    local set = {}
-    mem[key] = set
-    attr[set] = {
-      group = opts.group,
-      buffer = opts.buffer,
-    }
-    return setmetatable(set, eventset_mt)
-  end
-end
-
-do
-  local mem = setmetatable({}, { __mode = "v" })
-
-  get_autocmd_obj = function(opts)
-    validate { opts = { opts, "t" } }
-    local key = keygen(opts)
-    if mem[key] then return mem[key] end
-    local au = {}
-    mem[key] = au
-    attr[au] = {
-      group = opts.group,
-      buffer = opts.buffer,
-      eventset = get_eventset_obj(opts)
-    }
-    return setmetatable(au, autocmd_mt)
-  end
+function EventProxy:__call(action, opts)
+  return AutocmdManager(self, private[self].name, action, opts)
 end
 
 
+--
+--
+--        group_manager
+--
+--
 
---
---
---        group_mt
---
---
+GroupManager.get = AutocmdManager.get
+GroupManager.exec = AutocmdManager.exec
 
-group_mt.get = autocmd_mt.get
-group_mt.exec = autocmd_mt.exec
-
-function group_mt:create()
-  return a.nvim_create_augroup(self.name, { clear = false })
+function GroupManager:create()
+  return a.nvim_create_augroup(private[self].name, { clear = false })
 end
 
-function group_mt:clear(opts)
+function GroupManager:clear(opts)
   if not opts then
-    return a.nvim_create_augroup(self.name, { clear = true })
+    return a.nvim_create_augroup(private[self].name, { clear = true })
   else
-    autocmd_mt.clear(self, opts)
+    AutocmdManager.clear(self, opts)
   end
 end
 
-function group_mt:del()
-  a.nvim_del_augroup_by_name(self.name)
+function GroupManager:del()
+  a.nvim_del_augroup_by_name(private[self].name)
   self = nil
 end
 
-function group_mt:define(spec)
+function GroupManager:define(spec)
   validate { spec = { spec, "f" } }
   return spec(self.au)
 end
 
-function group_mt:__index(k)
+function GroupManager:__index(k)
   if k == "id" then
-    return group_mt.create(self)
-  elseif k == "name" or k == "group" then
-    return attr[self].scope.group
-  elseif k == "scope" then
-    return attr[self].scope
-  elseif k == "au" or k == "autocmd" then
-    return attr[self].autocmd
+    return self:create()
+  elseif k == "name" then
+    return private[self].name
+  elseif k == "au" then
+    return private[self].au
+  end
+end
+
+GroupManager.__call = GroupManager.define
+
+
+local function assert_valid_event(name)
+  local valid, cmds = pcall(a.nvim_get_autocmds, { event = name })
+  if not valid then
+    error(string.format("'%s' is not a valid event name", name), 2)
+  elseif vim.tbl_isempty(cmds) then
+    return name
   else
-    return rawget(group_mt, k)
+    return cmds[1].event          -- camel-cased
   end
 end
 
-function group_mt:__newindex(k, v)
-  if k == "id"
-    or k == "name"
-    or k == "scope"
-    or k == "au"
-    or k == "autocmd"
-    then error("attempting to modify a read-only field", 2)
-  else
-    rawset(self, k, v)
-  end
+local function create_event_proxy(name, ctx)
+  name = assert_valid_event(name)
+  local event = setmetatable({}, { __index = EventProxy })
+  private[event] = {
+    name = name,
+    ctx = ctx,
+  }
+  return event
 end
 
-group_mt.__call = group_mt.define
+local function create_eventindex(ctx)
+  return setmetatable({}, {
+    __mode = "v",
+    __index = function(self, k)
+      k = k:lower()
+      local event = rawget(self, k)
+      if not event then
+        event = create_event_proxy(k, ctx)
+        self[k], self[k:lower()] = event, event
+      end
+      return event
+    end,
+  })
+end
 
+local function create_autocmd_manager(ctx)
+  local au = setmetatable({}, { __index = AutocmdManager })
+  private[au] = {
+    eventindex = create_eventindex(ctx)
+    ctx = ctx,
+  }
+  return au
+end
 
+local function create_group_manager(ctx)
+  local aug = setmetatable({}, { __index = GroupManager })
+  private[aug] = {
+    name = ctx.group,
+    au = create_autocmd_manager(ctx),
+    ctx = ctx,
+  }
+  return aug
+end
 
---
---
---        get_group_obj
---
---
-
-do
-  local mem = setmetatable({}, { __mode = "v" })
-
-  get_group_obj = function(k)
-    validate { group_name = { k, "s" } }
-    if mem[k] then return mem[k] end
-    local group = {}
-    mem[k] = group
-    attr[group] = {
-      opts = {
-        group = k
-      }
-    }
-    attr[group].autocmd = get_autocmd_obj(attr[group].opts)
-    return setmetatable(group, group_mt)
-  end
+local function create_groupindex()
+  return setmetatable({}, {
+    __index = function(self, k)
+      local aug = mem[k]
+      if not aug then
+        local ctx = context:new { group = name }
+        aug = create_group_manager(ctx)
+        mem[k] = aug
+      end
+      return aug
+    end,
+  })
 end
 
 
 
-M.cmd = get_autocmd_obj {}
-M.group = setmetatable({}, { __index = function(_, k) return get_group_obj(k) end, })
-M.events = get_eventset_obj
+M.group = create_groupindex()
+
+M.cmd = create_autocmd_manager(context:new({ group = "default" }))
 
 return M
