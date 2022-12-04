80e7f6ec64cfbb1ddc35c6c8169a7cf95a192fbd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-03 19:54:40 -0500

    Fix test helpers

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -9,18 +9,21 @@ M.api = setmetatable({}, {
 })
 
 function M.clear_all(group)
-  api.clear_autocmds { group = group }
-  api.clear_autocmds {}
+  M.api.clear_autocmds { group = group }
+  M.api.clear_autocmds {}
 end
 
 function M.get_test_autocmds(group)
-  return api.get_autocmds { event = "User", group = group }
+  return M.api.get_autocmds { event = "User", group = group }
 end
 
 function M.create_test_autocmds(group)
+  if group then
+    M.api.create_augroup(group, {})
+  end
   local cb = function() end
   for i = 1, 10 do
-    api.create_autocmd("User", {
+    M.api.create_autocmd("User", {
       callback = cb,
       group = group,
       pattern = string.format("testpattern%d", i),
