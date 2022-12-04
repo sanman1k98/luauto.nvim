1f080de61f0957b5780bf8af6610a8c4f390504d
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-12-03 19:55:06 -0500

    More big changes
    - refactor "methods" fields into their own classes
    - remove "new" table that contained constructor funcs
    - add some documentation
    
    TODO: think about renaming the `_ctx` field to `_opts` instead.

--- lua/luauto/init.lua
+++ lua/luauto/init.lua
@@ -1,88 +1,115 @@
 
-local methods = {
-  autocmd = {},   -- but-local and global
-  proxy = {},     -- event and pattern
-  augroup = {},
-}
+local Autocmd = {}
+local Event = {}
+local Augroup = {}
 
 local a, validate = vim.api, vim.validate
 
+---@class opts @Dictionary of autocommand options.
+---@field group string|number|nil: group name or id
+---@field buffer number|boolean|nil: true for the current buffer or a specific buffer number
+---@field pattern string|nil: pattern or patterns
+---@field desc string|nil: description of the autocommand
+---@field once boolean|nil: run the autocommand only once (defaults to false)
+---@field nested boolean|nil: run nested autocommands (default to false)
 
 ---@class context
 ---@field group string|number|nil: group name or id
 ---@field buffer number|boolean|nil: true for the current buffer or a specific buffer number
 ---@field pattern string|nil: pattern or patterns
 
-local function create_autocmd_tbl(ctx)
-  local self = setmetatable({}, methods.autocmd)
-  self._ctx = ctx or {}
-  return self
-end
-
-local function create_proxy_tbl(event, ctx)
-  local self = setmetatable({}, methods.proxy)
-  self._event = event
-  self._ctx = ctx
-  return self
-end
-
-local function create_augroup_tbl(group)
-  local self = setmetatable({}, methods.augroup)
-  self._group = group
-  self._autocmd = create_autocmd_tbl({ group = group })
-  return self
-end
-
-local function unsupported(op, msg)
-  local m = string.format("the operation '%s' is not supported.\n %s", op, msg)
-  error(m, 2)
-end
-
+---@private
 local function merge_opts(...)
   local opts = vim.tbl_extend("force", ...)
   if opts.buffer and opts.pattern then
     error("cannot specify a pattern for a buf-local autocmd", 2)
-  elseif opts.buffer == true then
+  elseif opts.buffer == true or opts.buffer == 0 then
     opts.buffer = a.nvim_get_current_buf()
-  elseif not opts.buffer then
-    opts.buffer = nil
   end
   return opts
 end
 
-local valid_events = setmetatable({}, {
-  __index = function(self, k)
-    local validity = pcall(a.nvim_get_autocmds, { event = k })
-    rawset(self, k, validity)
-    return validity
-  end,
-})
 
 
+--      constructors
 
-function methods.autocmd:__index(k)
-  if k == "buf" and not self._ctx.buffer then
-    local ctx = merge_opts(self._ctx, { buffer = true })
-    return new.autocmd(ctx)
-  elseif self._ctx.buffer == true and type(k) == "number" then
-    local ctx = merge_opts(self._ctx, { buffer = k })
-    return new.autocmd(ctx)
-  end
-
-  local v = methods.autocmd[k]
-  if v then return v end
-
-  local event = k:lower()   -- event names are case-insensitive
-  if not valid_events[event] then
-    error(string.format("'%s' is not a valid event name", event), 2)
-  end
-
-  return create_proxy_tbl(event, self._ctx)
+---@param ctx context
+---@private
+local function create_autocmd_object(ctx)
+  local self = {
+    _ctx = ctx
+  }
+  return setmetatable(self, Autocmd)
 end
 
-function methods.autocmd:__call(event, action, opts)
+local function create_event_object(event, ctx)
+  local self = {
+    _event = event,
+    _ctx = ctx,
+  }
+  return setmetatable(self, Event)
+end
+
+local function create_augroup_object(name)
+  local ctx = { group = name }
+  local self = {
+    _au = create_autocmd_object(ctx),
+    _ctx = ctx,
+  }
+  return setmetatable(self, Augroup)
+end
+
+
+
+--      autocmd methods
+
+function Autocmd:__index(k)
+  if k == "buf" then
+    return setmetatable({ _ctx = self._ctx }, {
+      __index = function(t, buffer)
+        validate { buffer = { buffer, {"b", "n"} } }
+        return create_autocmd_object(merge_opts(t._ctx, { buffer = buffer }))
+      end,
+    })
+  else
+    validate { k = { k, {"s", "t"} } }
+    return Autocmd[k] or create_event_object(k, self._ctx)
+  end
+end
+
+---@param opts opts: optional dictionary of autocommand options
+function Autocmd:get(opts)
+  validate { opts = { opts, "t", true } }
+  return a.nvim_get_autocmds(merge_opts(opts or {}, self._ctx))
+end
+
+---@param opts opts: optional dictionary of autocommand options
+function Autocmd:clear(opts)
+  validate { opts = { opts, "t", true } }
+  a.nvim_clear_autocmds(merge_opts(opts or {}, self._ctx))
+end
+
+--      event and pattern proxy methods
+
+function Event:__index(k)
+  -- check if key is a method name
+  local v = Event[k]
+  if v then return v end
+
+  -- check if we can specify the pattern
+  if not (self._ctx.buffer or self._ctx.pattern) then
+    local ctx = merge_opts(self._ctx, { pattern = k })
+    return create_event_object(self._event, ctx)
+  end
+
+  return nil
+end
+
+--- Create an autocmd for the event or events.
+---@param action string|function: a callback or command to be executed when the autocommand triggers
+---@param opts table}nil:
+function Event:__call(action, opts)
   validate {
-    event = { event, {"s", "t"} },
     action = { action, {"s", "f"} },
     opts = { opts, "t", true },
   }
@@ -95,113 +122,74 @@ function methods.autocmd:__call(event, action, opts)
     opts.callback = action
   end
 
-  return a.nvim_create_autocmd(event, opts)
+  return a.nvim_create_autocmd(self._event, opts)
 end
 
---- Use for defining buf-local autocmds
-function methods.autocmd:define(spec)
-  if not self._ctx.buffer then
-    unsupported("define", "use 'define' for buf-local autocmds")
-  end
-  validate { spec = { spec, "f" } }
-  return spec(self)
-end
-
-function methods.autocmd:exec(event, opts)
-  validate {
-    event = { event, {"s", "t"} },
-    opts = { opts, "t", true },
-  }
-  a.nvim_exec_autocmds(event, merge_opts(opts or {}, self._ctx))
-end
-
-function methods.autocmd:get(opts)
+--- Execute autocmds matching this event or events
+---@param opts table|nil:
+function Event:exec(opts)
   validate { opts = { opts, "t", true } }
-  return a.nvim_get_autocmds(merge_opts(opts or {}, self._ctx))
+  opts = merge_opts(opts or {}, self._ctx)
+  return a.nvim_exec_autocmds(self._event, opts)
 end
 
-function methods.autocmd:clear(opts)
-  validate { opts = { opts, "t", true } }
-  a.nvim_clear_autocmds(merge_opts(opts or {}, self._ctx))
-end
-
---      event and pattern proxy methods
-
---- Create an autocmd for the event
----@param
----@param
-function methods.proxy:__call(action, opts)
-  return methods.autocmd.__call(self, self._event, action, opts)
-end
-
---- Execute autocmds matching this event
----@param
-function methods.proxy:exec(opts)
-  return methods.autocmd.exec(self, opts)
-end
-
-function methods.proxy:get(opts)
+--- Get autocmds for the event
+---@param opts table|nil:
+function Event:get(opts)
   validate { opts = { opts, "t", true } }
   opts = merge_opts(opts or {}, self._ctx)
   opts.event = self._event
   return a.nvim_get_autocmds(opts)
 end
 
-function methods.proxy:clear(opts)
+function Event:clear(opts)
   validate { opts = { opts, "t", true } }
   opts = merge_opts(opts or {}, self._ctx)
   opts.event = self._event
   return a.nvim_clear_autocmds(opts)
 end
 
-function methods.proxy:__index(k)
-  local v = methods.proxy[k]
-  if v then return v end
-
-  if self._ctx.buffer or self._ctx.pattern then return nil end
-
-  local ctx = merge_opts(self._ctx, { pattern = k })
-  return create_proxy_tbl(self._event, ctx)
-end
-
 --      augroup methods
 
-function methods.augroup:create(opts)
-  validate { opts = { opts, "t", true } }
-  opts = opts or { clear = false }
-  return a.nvim_create_augroup(self._group, opts)
-end
-
-methods.augroup.get = methods.autocmd.get
-methods.augroup.exec = methods.autocmd.exec
-methods.augroup.clear = methods.autocmd.clear
-
-function methods.augroup:define(spec)
-  validate { spec = { spec, "f" } }
-  self:create({ clear = false })
-  return spec(self._autocmd)
-end
-
-function methods.augroup:__index(k)
-  if k == "id" then
-    return self:create({ clear = false })
+function Augroup:__index(k)
+  if k == "id" then         -- return group id number
+    return a.nvim_create_augroup(self._ctx.group, { clear = false })
   else
-    return methods.augroup[k] or self._autocmd[k]
+    return Augroup[k]
   end
 end
 
+--- Define autocommands in the augroup by calling it with a spec function as
+--- the argument. It will call |nvim_create_augroup()|, then calls your spec
+--- function with an Autocmd object that has the group anemas the only argument.
+---@param spec function: a function that defines one parameter which is used in the body to create autocmds
+function Augroup:__call(spec)
+  validate { spec = { spec, "f" } }
+  a.nvim_create_augroup(self._ctx.group, { clear = false })
+  return spec(self._au)
+end
+
+function Augroup:create(opts)
+  validate { opts = { opts, "t", true } }
+  opts = opts or { clear = false }
+  return a.nvim_create_augroup(self._ctx.group, opts)
+end
+
+function Augroup:del()
+  a.nvim_del_augroup_by_name(self._ctx.group)
+end
+
+Augroup.get = Autocmd.get
+Augroup.clear = Autocmd.clear
 
 
-return {
-  cmd = create_autocmd_tbl({
-    group = nil,      -- default group
-    buffer = nil,     -- global
-  }),
 
-  -- indexable by group names
-  group = setmetatable({}, {
-    __index = function(_, k)
-      return create_augroup_tbl(k)
-    end,
-  }),
-}
+local M = create_autocmd_object({})
+
+M.group = setmetatable({}, {
+  __index = function(_, name)
+    return create_augroup_object(name)
+  end,
+})
+
+return M
